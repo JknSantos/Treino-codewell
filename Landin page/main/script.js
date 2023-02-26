3933dbcf39358eb2b47f06196e2dd23076a1311d
Nicolas Gomes de Almeida <109389725+RebelAstronomer@users.noreply.github.com>
2023-02-25 21:16:00 -0300

    inicialização da função started

--- Landin page/main/script.js
+++ Landin page/main/script.js
@@ -1,3 +1,10 @@
 function started() {
+    const $emailInput = document.getElementById('email');
     
+    // Checando se há algo escrito no input de email
+    if ($emailInput.value != '') {
+        console.log('com email');
+    } else {
+        console.log('sem email');
+    }
 }
