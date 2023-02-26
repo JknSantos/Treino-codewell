b51c6e6f32151c3c95088d8e452a546df670a81b
Nicolas Gomes de Almeida <109389725+RebelAstronomer@users.noreply.github.com>
2023-02-25 21:41:12 -0300

    inicialização da função que mostra o form e criação das classes do CSS

--- Landin page/main/script.js
+++ Landin page/main/script.js
@@ -1,10 +1,23 @@
+// Função para iniciar o login de usuario
 function started() {
     const $emailInput = document.getElementById('email');
+    const $form = document.querySelector('.form');
     
+    // Mostrando a forms
+    $form.classList.toggle('form-show');
+
     // Checando se há algo escrito no input de email
     if ($emailInput.value != '') {
-        console.log('com email');
-    } else {
-        console.log('sem email');
+        showForm($emailInput.value);
+    } 
+}
+
+// Função para mostrar o forms
+function showForm(email) {
+    const $login = document.getElementById('login');
+
+    // Pegando o email já digitado
+    if (email != '') {
+        $login.value = email;
     }
 }
