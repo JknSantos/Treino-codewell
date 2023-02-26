20ffb7aa12f9889f1addd6f319c9728decdad7e1
Nicolas Gomes de Almeida <109389725+RebelAstronomer@users.noreply.github.com>
2023-02-25 22:27:01 -0300

    Update Scripts.js
    - Criação das funções para checagem dos login de usuário

--- Landin page/main/script.js
+++ Landin page/main/script.js
@@ -1,3 +1,23 @@
+// Adicionando um evento para quando apertar enter no input do email
+// EMAIL
+document.getElementById('email').addEventListener('keypress', function(event) {
+    if (event.key == 'Enter') {
+        started();
+    }
+});
+// LOGIN
+document.getElementById('login').addEventListener('keypress', function(event) {
+    if (event.key == 'Enter') {
+        checkLogin();
+    }
+})
+// PASSWORD
+document.getElementById('password').addEventListener('keypress', function(event) {
+    if (event.key == 'Enter') {
+        checkLogin();
+    }
+})
+
 // Função para iniciar o login de usuario
 function started() {
     const $emailInput = document.getElementById('email');
@@ -8,16 +28,28 @@ function started() {
 
     // Checando se há algo escrito no input de email
     if ($emailInput.value != '') {
-        showForm($emailInput.value);
+        applyEmail($emailInput.value);
     } 
 }
 
 // Função para mostrar o forms
-function showForm(email) {
+function applyEmail(email) {
     const $login = document.getElementById('login');
 
     // Pegando o email já digitado
     if (email != '') {
         $login.value = email;
+    }
+}
+
+// Função para checar se todos os campos de login foram preenchidos
+function checkLogin() {
+    const $login = document.getElementById('login');
+    const $password = document.getElementById('password');
+
+    if ($login.value != '' && $password.value != '') {
+        alert('Login completed');
+    } else {
+        alert('One of the fields has not been filled in correctly');
     }
 }
